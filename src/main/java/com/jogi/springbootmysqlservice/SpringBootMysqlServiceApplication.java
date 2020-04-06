837364fac69749e02d3e8fd5cf4eae3dcf3444ca
jogeswar13 <jogeswars13@gmail.com>
2020-04-06 11:08:41 +0530

    Initial changes.

---
+++ src/main/java/com/jogi/springbootmysqlservice/SpringBootMysqlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jogi.springbootmysqlservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMysqlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMysqlServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jogi/springbootmysqlservice/SpringBootMysqlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jogi.springbootmysqlservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMysqlServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
